ae6826a081d202e0dfdd2e31010e37c1ea9079b4
Cristian <cristian.onate1901@alumnos.ubiobio.cl>
2022-12-27 21:13:05 -0300

    actualizacion 2

--- sudoku.c
+++ sudoku.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int n =9;
 int m = 9;
+int matriz[9][9];
+int matriz2[9][9];
 
-void mostrar(int matriz[n][n]){ // imprime matriz por consola
+void posm(int *matriz[n][n]){ //crea una matriz con valores de posiciones
+	int i,j,aux;
+	char numero[10];
+	char numero2[4];
+	for(i=0;i<=9;i++){
+		for(j=0;j<=9;j++){
+			sprintf(numero,"%d",i);
+			sprintf(numero2,"%d",j);
+			strcat(numero,numero2);
+			aux = strtol(numero,NULL,10);
+			matriz[i][j] = aux;
+		}
+	}
+}
+void mostrar(int *matriz[n][n]){ // imprime matriz por consola
 	int i, j= 0;
   	for(i=0;i<n;i++){
     	for(j=0;j<n;j++){
-      		printf("%d ",matriz[i][j]);
+      		printf(" %d ",matriz[i][j]);
     	}
     printf("\n");
   	}
   	printf("\n");
 }
-
-
-void azar(int matriz[n][n]){ //agregar numeros al alzar a la matriz
+void azar(int *matriz[n][n]){ //agregar numeros al alzar a la matriz
 	int i, j,c;
 	int mtemporal=0;
 	for(c=0;c<9;c++){
@@ -31,8 +46,7 @@ void azar(int matriz[n][n]){ //agregar numeros al alzar a la matriz
 		}
 	}
 }
-
-void llenar(int m[n][n]){ // setea la matriz con 0
+void llenar(int *m[n][n]){ // setea la matriz con 0
   int i, j;
   for(i=0;i<n;i++){
     for(j=0;j<n;j++){
@@ -41,28 +55,38 @@ void llenar(int m[n][n]){ // setea la matriz con 0
   }
   azar(m);
   mostrar(m);
+  printf("\nMatriz azar\n");
 }
-
-int rechace(int matriz[m][n],int x, int y){
-	int numeroazar,i;
-	int cont = 0;
-	int j= 1;
-	if(matriz[x][y] == 0){
-		while(j!=0){
-			int numeroazar = rand()%10;
+int rechace(int nodo){
+	char aux[2];
+	int ayu;
+	sprintf(aux,"%d",nodo);
+	int numeroazar,i,auxi,h,cont,j;
+	if(matriz[aux[0]][aux[1]] == 0){
+		numeroazar = rand()%10;
+		j=1;
+		while(j == 1){
+			cont = 0;
 			for(i=0;i<=9;i++){
-				if(numeroazar == matriz[i][y]){
+				if(numeroazar == matriz[i][aux[1]]){
 					cont++;
 				}
-				if(numeroazar == matriz[x][i]){
+				if(numeroazar == matriz[aux[0]][i]){
 					cont++;
 				}
 			}
-			if(cont == 0){
-				printf("rechace matriz %d\n",matriz[x][y]);
-				matriz[x][y]=numeroazar;
-				mostrar(matriz);
-				system("pause");
+			if(cont > 0){
+				printf("rechace matriz %d\n",matriz[aux[0]][aux[1]]);
+				auxi = numeroazar;
+				h=0;
+				while(h == 0){
+					numeroazar = rand()%10;
+					if(numeroazar != auxi){
+						h = 1;
+					}
+				}
+			}else{
+				matriz[aux[0]][aux[1]] = numeroazar;
 				j=0;
 				return 1;
 			}
@@ -70,7 +94,7 @@ int rechace(int matriz[m][n],int x, int y){
 	}
 	return 0;
 }
-int aceptar(int matriz[m][n]){
+int aceptar(int nodo){
 	int i,j;
 	int cont = 0;
 	for(i=0;i<=9;i++){
@@ -81,30 +105,61 @@ int aceptar(int matriz[m][n]){
 		}
 	}
 	if(cont > 0){
-		return 1;
+		return 0;
 	}
 	else{
-		return 0;
+		return 1;
 	}
 	
 }
-int primero(int matriz[m][n]){
-	int x = 0;
-	int y = 0;
-	return;
+
+//marcar matriz de prueba con posiciones usadas
+int primero(int nodo){
+	char aux[2];
+	int ayu;
+	sprintf(aux,"%d",nodo);
+	
+	if(matriz2[aux[0]][aux[1]] != "\0"){ //arreglar con respecto a nodo como var de entrada
+		matriz2[aux[0]][aux[1]] = "\0";
+		ayu = strtol(aux,NULL,10);
+		return ayu;
+	}else{
+		ayu = strtol(aux,NULL,10);
+		return ayu; 
+	}
 }
-int siguiente(int matriz[m][n]){
-	if(m == 9){
-		m=0;
+//agregar paso de poscion
+int siguiente(int nodo){
+	char aux[2];
+	int ayu;
+	sprintf(aux,"%d",nodo);
+	printf("%d\n",aux);
+	if(matriz2[aux[0]][aux[1]] == "\0"){
+		aux[0]=+1;
+		aux[1]=+1;
+		if(aux[0] > 9 ){
+			aux[0]=0;
+			if(matriz2[aux[0]][aux[1]] != "\0"){
+				matriz2[aux[0]][aux[1]]="\0";				
+			}
+		}
+		if(aux[1] > 9){
+			aux[1]=0;
+			if(matriz2[aux[0]][aux[1]] !="\0"){
+				matriz2[aux[0]][aux[1]]="\0";
+			}
+		}
+		ayu = strtol(aux,NULL,10);
+		return ayu;		
+	}else{
+		return nodo;
 	}
-	if(n=9){
-		n=0;
-	}
-	return matriz[m][n];
 }
 
-void backtracking(int *nodo[n][n],int x,int y){ //resulve la matriz por recursion
-   if(rechace(nodo,x,y) == 1){
+// modificar funcion siguiente
+
+void backtracking(int nodo){ //resulve la matriz por recursion
+   if(rechace(nodo) == 1){
    		return nodo;
    		printf("rechace retorna %d",nodo);
    		system("pause");
@@ -115,11 +170,15 @@ void backtracking(int *nodo[n][n],int x,int y){ //resulve la matriz por recursio
       	mostrar(nodo); //Imprime salida
       	exit(0);
    }
-   int hijo = nodo[x][y];
-   while(hijo != null){
+      
+   int hijo = primero(nodo);
+
+   while(hijo == 0){
+   	  printf("\n while\n"); //areglar la funcion hijo
+      system("pause");
       backtracking(hijo);
       hijo = siguiente(hijo);
-   }
+	}
 }
 
 /*
@@ -138,13 +197,14 @@ void backtracking(<tipo> nodo){ //ejmplo de uso
 */
 
 int main() { // funcion inicial
-	
-	int matriz [n][n];
-	int m;
-	llenar(&matriz);
-	backtracking(matriz);
+	int a = 00;
+	llenar(matriz);
+	posm(matriz2);
+	backtracking(a);
 	return 0;
+	
 }
+
 
 
 
